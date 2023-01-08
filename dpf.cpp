e4d99b221129c493667319b62cb4d4716bef0df2
Naman Kumar <namankr02@gmail.com>
2023-01-08 11:42:43 +0530

    restructured dpf_layer, separated t

--- dpf.cpp
+++ dpf.cpp
@@ -6,12 +6,18 @@ struct dpf_layer {
     size_t size;
     size_t level;
     block *nodes;
-    block z[4];
+    uint8_t *prevt;
+    uint8_t *currt;
+    block zs[2];
+    uint8_t zt[2];
 };
 
 void free_dpf_layer(dpf_layer *dpfl) {
     free(dpfl->nodes);
-    free(dpfl->z);
+    free(dpfl->prevt);
+    free(dpfl->currt);
+    free(dpfl->zs);
+    free(dpfl->zt);
     free(dpfl);
 };
 
@@ -24,13 +30,14 @@ void convert(const int inp_bitwidth,
 
 void prg_eval_all_and_xor(dpf_layer *dpfl, block *ct, const block *pt) {
 
-    block* output_nodes = (block*)malloc(4*dpfl->size*sizeof(block));
+    block* output_nodes = (block*)malloc(2*dpfl->size*sizeof(block));
+    uint8_t* output_t = (uint8_t*)malloc(2*dpfl->size*sizeof(uint8_t));
 
     if (dpfl->level==0) {
-        dpfl->z[0] = ZeroBlock;
-        dpfl->z[1] = ZeroBlock;
-        dpfl->z[2] = ZeroBlock;
-        dpfl->z[3] = ZeroBlock;
+        dpfl->zs[0] = ZeroBlock;
+        dpfl->zs[1] = ZeroBlock;
+        dpfl->zt[0] = 0;
+        dpfl->zt[1] = 0;
     }
 
     #pragma omp parallel for
@@ -42,22 +49,26 @@ void prg_eval_all_and_xor(dpf_layer *dpfl, block *ct, const block *pt) {
 
         // encrypt
         aes_key.ecbEncFourBlocks(pt, ct);
-        output_nodes[4*i] = ct[0];
-        output_nodes[4*i+1] = toBlock(0,lsb(ct[1]));
-        output_nodes[4*i+2] = ct[2];
-        output_nodes[4*i+3] = toBlock(0,lsb(ct[3]));
+        output_nodes[2*i] = ct[0];
+        output_nodes[2*i+1] = ct[2];
+        output_t[2*i] = lsb(ct[1]);
+        output_t[2*i+1] = lsb(ct[3]);
 
         // set xor
-        dpfl->z[0] = dpfl->z[0] ^ ct[0];
-        dpfl->z[1] = dpfl->z[1] ^ toBlock(0,lsb(ct[1]));
-        dpfl->z[2] = dpfl->z[2] ^ ct[2];
-        dpfl->z[3] = dpfl->z[3] ^ toBlock(0,lsb(ct[3]));
+        dpfl->zs[0] = dpfl->zs[0] ^ ct[0];
+        dpfl->zs[1] = dpfl->zs[1] ^ ct[2];
+        dpfl->zt[0] = dpfl->zt[0] ^ lsb(ct[1]);
+        dpfl->zt[1] = dpfl->zt[1] ^ lsb(ct[3]);
     }
 
     free(dpfl->nodes);
     dpfl->nodes = output_nodes;
 
-    dpfl->size *= 4;
+    free(dpfl->prevt);
+    dpfl->prevt = dpfl->currt;
+    dpfl->currt = output_t;
+
+    dpfl->size *= 2;
     dpfl->level += 1;
 };
 
@@ -94,7 +105,7 @@ std::pair<dpf_key, dpf_key> dpf_keygen(int height,
     // set AES ciphertext
     block ct[4];
 
-#ifdef DPF_PROFILE
+#ifdef DPF_PROFILE_
     printf("START DPF LEVEL 0 %d\n", current_timestamp());
 #endif
 
@@ -106,26 +117,30 @@ std::pair<dpf_key, dpf_key> dpf_keygen(int height,
     dpfl0->level = 0;
     dpfl0->nodes = (block*)malloc(sizeof(block));
         dpfl0 -> nodes[0] = s[0];
+    dpfl0->currt = (uint8_t*)malloc(sizeof(uint8_t));
+        dpfl0 -> currt[0] = 0;
 
     dpfl1->size = 1;
     dpfl1->level = 0;
     dpfl1->nodes = (block*)malloc(sizeof(block));
         dpfl1 -> nodes[0] = s[1];
+    dpfl1->currt = (uint8_t*)malloc(sizeof(uint8_t));
+        dpfl1 -> currt[0] = 1;
 
 
     for (int i = 0; i < height; i++) {
 
-#ifdef DPF_PROFILE
+#ifdef DPF_PROFILE_
     printf("START DPF LEVEL %d%d\n", dpfl0->level, current_timestamp());
 #endif
         prg_eval_all_and_xor(dpfl0, ct, pt);
         prg_eval_all_and_xor(dpfl1, ct, pt);
 
-        const uint8_t sig = (static_cast<uint8_t>((index[0].value ^ index[1].value) >> (group_bitwidth - 1 - i)) & 1 ^ 1 == 0) ? 0 : 2;
+        const uint8_t sig = static_cast<uint8_t>((index[0]+index[1]).value >> (group_bitwidth - 1 - i)) & 1 ^ 1;
 
-        sigma[i] = dpfl0->z[sig] ^ dpfl1->z[sig];
-        tau0[i] = dpfl0->z[0] ^ toBlock(index[0].value) ^ dpfl1->z[0] ^ toBlock(index[0].value) ^ toBlock(1);
-        tau1[i] = dpfl0->z[1] ^ toBlock(index[2].value) ^ dpfl1->z[1] ^ toBlock(index[2].value) ^ toBlock(1);
+        sigma[i] = dpfl0->zs[sig] ^ dpfl1->zs[sig];
+        tau0[i] = dpfl0->zs[0] ^ toBlock(index[0].value) ^ dpfl1->zs[0] ^ toBlock(index[0].value) ^ toBlock(1);
+        tau1[i] = dpfl0->zs[1] ^ toBlock(index[1].value) ^ dpfl1->zs[1] ^ toBlock(index[1].value) ^ toBlock(1);
 
     }
 };
